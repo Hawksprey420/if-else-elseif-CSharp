b352568de7d25e42a8b36258c0c9723bb8b96f93
Hawksprey <75325547+Hawksprey420@users.noreply.github.com>
2022-09-15 15:27:31 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace CSharpRegistrationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+        
+            {
+                Console.WriteLine("Seems you're old enough");
+            }
+            else if (age >=17)
+            {
+                Console.WriteLine("Seems you need to get permission I guess?");
+            }
+            else
+            {
+                Console.WriteLine("You are not old enough");
+            }
+
+        }
+    }
+}
